042fcf2b4f80c454032ccc9ea3ea0b33c2e540b2
cmaclen2 <cmaclen2@id415m11.cs.unb.ca>
2018-09-27 14:58:21 -0300

    THIS COMMITED - CARTER ME - CARTER

--- newproj/src/SampleMain.java
+++ newproj/src/SampleMain.java
@@ -4,7 +4,7 @@ public class SampleMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < 1; i++) {
-			System.out.println("Hello World!");
+			System.out.println("Hello Aadennnnn");
 		}
 	}
 }
